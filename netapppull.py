01bd1dae4ee98945bb420066277b8b5d1abd9b60
root <root@localhost.localdomain>
2020-03-23 15:25:19 +0700

    initial commit

--- netapppull.py
+++ netapppull.py
@@ -7,63 +7,68 @@ import urllib3
 urllib3.disable_warnings()
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 class netapp:
-  def netapp(self):
-    api_base_url =  "https://192.168.1.35"
-    url= api_base_url+'/rest/events?limit=20'
-    print(url)
-    passw = 'P@ssw0rd123'
-    print(passw)
-    headers = {
-      'Accept' : 'application/vnd.netapp.object.inventory.hal+json',
-      'Authorization': 'Basic YWRtaW46UEBzc3cwcmQxMjM='
-    }
-    mydata = json.dumps({'username':'admin','password':'P@ssw0rd123'})
-    s = requests.Session()
-    print("-------------s:------------------",s)
-    s.auth = ('admin',passw)
-    rp = s.post(url=api_base_url,data=mydata,verify=False)
-    rs =  s.get(url=url, verify=False,headers=headers)
-    print("-------------rs:------------------",rs)
-    #result = rs.text
-    #print(result.encode('utf8'))
-    if rs.status_code == 200:
-        dictdump = json.loads(rs.text)
-        print("-------------------------------------------------------------------------------------------------------------------")
-        print(dictdump)
-        print("----------------------------------------------------------")
-        sl = dictdump['_embedded'].get('netapp:aggregateInventoryList')
-        print(sl)
-        for x in sl:
-            print(str(x))
-            d = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+      
+      def __init__(self):
+            self.api_base_url =  "https://192.168.1.35"
+            self.url= self.api_base_url+'/rest/events'
+            print(self.url)
+            self.headers = {
+              'Accept' : 'application/vnd.netapp.object.inventory.hal+json',
+              'Authorization': 'Basic YWRtaW46UEBzc3cwcmQxMjM='
+            }
+            
+      def netappconnect(self):
+            s = requests.Session()
+            rs =  s.get(url=self.url, verify=False,headers=self.headers)
+            if rs.status_code == 200:
+                self.dictdump = json.loads(rs.text)
+                #print(self.dictdump)
+                print("-------------------------------------------------------------------------------------------------------------------")
+                print("----------------------------------------------------------")
+                sl = self.dictdump['_embedded'].get('netapp:aggregateInventoryList')
+                print(sl)
+                return self.dictdump
+                
 class elk:
       
-  def esconnect(self):
-      es_header = [{
+      
+  def __init__(self):
+      self.es_header = [{
         'host': '192.168.1.168',
         'port': '9200'
       }]
-      id = 'netapp001'
+      self.id = 'netapp001'
+      self.index = 'netapp-index'
+  def esconnect(self):
       try:
-        self.esc = Elasticsearch(es_header)
+        self.esc = Elasticsearch(self.es_header)
         self.esc.cluster.health(wait_for_status='yellow', request_timeout=2)
         print("successfull")     
       except Exception as ex:
         print("Error connecting es:",ex)
           
   def createindex(self):
-      if self.esc.indices.exists(index='netapp-index'):
+      self.esconnect()
+      if self.esc.indices.exists(index=self.index):
         print('index already exists')
       else:
             print('creating index')
-            self.esc.indices.create(index='netapp-index',ignore=400)
+            self.esc.indices.create(self.index,ignore=400)
             print('index created')
       #es.indices.create(index='test-index', ignore=400)
       #print(s)
+  def putdata(self,data):
+        self.esc.index(index=self.index,doc_type='events',body=data)
+        print("data posting completed")
+def main():
+      es = elk()
+      es.createindex()
+      ntapp = netapp()
+      data = ntapp.netappconnect()
+      es.putdata(data)
 
 if __name__ == "__main__":
+      main()
       
-  es = elk()
-  es.esconnect()
-  es.createindex()
+  
   
